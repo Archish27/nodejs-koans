82e778127d6ff7f40ef9e4562db6ec2b4c1e0718
Archish Thakkar <archishthakkar@gmail.com>
2021-09-04 20:03:14 +0530

    :sparkle: Initial Project setup

---
+++ README.md
@@ -0,0 +1,23 @@
+# Nodejs Koans
+
+The Nodejs Koans walk you along the path to enlightenment in order to learn JavaScript + Nodejs. The goal is to learn the JavaScript language & Nodejs, syntax, structure, and some common functions and libraries.
+
+## The Structure 🗃️
+
+The koans is broken into different files, strings are covered in `strings.js`. Similarly, arrays are touched in `arrays.js`. These files are then imported in `test/index.test.js`.
+
+Each koan builds up your knowledge of JavaScript and builds upon itself. It will stop at the first place you need to correct.
+
+Some koans simply need to have the correct answer substituted for an incorrect one. Some, however, require you to supply your own answer. If you see the method `__` (a double underscore) listed, it is a hint to you to supply your own code in order to make it work correctly.
+
+
+## Installation Steps 🔨
+
+- To install project execute : \
+`npm install`
+
+- Execute koans : \
+`npm run test --silent`
+
+
+[Inspiration Video](https://www.youtube.com/watch?v=LrhkUsRg3vo&ab_channel=OmniakaEverything) 💡
---
+++ util/reporter.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const chalk = require('chalk');
+const Mocha = require('mocha');
+const {
+  EVENT_RUN_BEGIN,
+  EVENT_RUN_END,
+  EVENT_TEST_FAIL,
+  EVENT_TEST_PASS,
+  EVENT_SUITE_BEGIN,
+  EVENT_SUITE_END
+} = Mocha.Runner.constants;
+
+class MyReporter {
+  constructor(runner) {
+    this._indents = 0;
+    const stats = runner.stats;
+
+    runner
+      .once(EVENT_RUN_BEGIN, () => {
+      })
+      .on(EVENT_SUITE_BEGIN, (test) => {
+        console.log(
+          `${this.indent()}${chalk.magenta(`${test.title}`)}`
+        );
+        this.increaseIndent();
+      })
+      .on(EVENT_SUITE_END, () => {
+        this.decreaseIndent();
+      })
+      .on(EVENT_TEST_PASS, test => {
+        console.log(
+          `${this.indent()}${chalk.green(`${test.title}`)}`
+        );
+      })
+      .on(EVENT_TEST_FAIL, (test, err) => {
+        console.log('Please meditate on the following code: 🧘')
+        console.log(
+          `${this.indent()}${chalk.red(`${test.title} in ${test.file}`)}`
+        );
+        console.log("\n");
+      })
+      .once(EVENT_RUN_END, () => {
+        const total = stats.passes * 100/ (stats.passes + stats.failures);
+        console.log(`The Master says:`)
+        if(total === 100){
+          console.log(chalk.green(`  Enlightenment achieved 😇`))
+        }
+        else{
+          console.log(chalk.cyan(`  You have not yet reached enlightenment 😌\n  Do not lose hope 🙏`))
+        }
+        console.log("\n");
+        console.log(chalk.bgBlue(`Your progress : ${stats.passes}/${stats.passes + stats.failures} (${~~total}%)`));
+      });
+  }
+
+  indent() {
+    return Array(this._indents).join('  ');
+  }
+
+  increaseIndent() {
+    this._indents++;
+  }
+
+  decreaseIndent() {
+    this._indents--;
+  }
+}
+
+module.exports = MyReporter;
